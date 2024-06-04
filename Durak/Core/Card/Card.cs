1b95e3a04a0fe46646b8a3c8b0757074576b8c5c
VardanMkrt4ian <vardan_mkrtchyan@edu.aua.am>
2024-06-04 19:30:07 +0400

    added Durak card game project

---
+++ Durak/Core/Card/Card.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Durak.Core.Card
+{
+    public sealed class Card : IComparable
+    {
+        public sealed class Suit
+        {
+            public enum Color
+            {
+                Red,
+                Black
+            }
+
+            public enum Symbol
+            {
+                Heart,
+                Diamond,
+                Club,
+                Spade
+            }
+
+            public Color SuitColor { get; }
+            public Symbol SuitSymbol { get; }
+
+            public Suit(Symbol symbol)
+            {
+                SuitSymbol = symbol;
+                SuitColor = IdentifyColor(symbol);
+            }
+
+            public Color IdentifyColor(Symbol symbol)
+            {
+                switch (symbol)
+                {
+                    case Symbol.Heart: return Color.Red;
+                    case Symbol.Diamond: return Color.Red;
+                    case Symbol.Club: return Color.Black;
+                    default: return Color.Black;
+                }
+            }
+            public override string ToString()
+            {
+                return SuitSymbol.ToString();
+            }
+        }
+
+        public enum Value
+        {
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King,
+            Ace
+        }
+
+        public Suit CardSuit { get; }
+        public Value CardValue { get; }
+
+        public Card(Suit suit, Value value)
+        {
+            CardSuit = suit;
+            CardValue = value;
+        }
+
+        public int CompareTo(Object other)
+        {
+            return 0;
+        }
+        public override string ToString()
+        {
+            return $"{CardValue} of {CardSuit}s";
+        }
+    }
+}
